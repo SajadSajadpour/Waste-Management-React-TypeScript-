4f8011bf191cb5394d38189ef1785123b6bfa848
Sajad Sajadpour <sajad.sajadpour@gmail.com>
2026-01-12 15:17:55 -0500

    Deploy build output to docs for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/foodcycler-dashboard-demo/",
+  base: "/Waste-Management-React-TypeScript-/",
   plugins: [react()],
   resolve: {
     alias: {
